bed0c89d866596bcbc151264aad83a39b7c7504b
lucas <>
2025-03-29 00:06:56 +0100

    update submit script

--- Project2/submit.sh
+++ Project2/submit.sh
@@ -13,19 +13,21 @@
 ### -- send notification at start --
 ###BSUB -B
 ### -- send notification at completion--
-#BSUB -N
+###BSUB -N
 ### -- Specify the output and error file. %J is the job-id %I is the job-array index --
 ### -- -o and -e mean append, -oo and -eo mean overwrite -- 
-#BSUB -o Output.out
-#BSUB -e ./error/Output_%J_%I.err 
+### BSUB -o ./log/Output_%J_%I.out
+### BSUB -e ./log/Output_%J_%I.err 
+# BSUB -o ./log/Output_%I.out
+# BSUB -e ./log/Output_%I.err 
 # here follow the commands you want to execute 
 # Program_name_and_options
 source /zhome/5f/1/167776/playground/bin/activate
 
-python Project2/ensemble_vae.py trainEnsamble --num-decoders 1 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
-python Project2/ensemble_vae.py trainEnsamble --num-decoders 2 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
-python Project2/ensemble_vae.py trainEnsamble --num-decoders 3 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
-python Project2/ensemble_vae.py trainEnsamble --num-decoders 4 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
+python ensemble_vae.py trainEnsamble --num-decoders 1 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
+python ensemble_vae.py trainEnsamble --num-decoders 2 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
+python ensemble_vae.py trainEnsamble --num-decoders 3 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
+python ensemble_vae.py trainEnsamble --num-decoders 4 --model-nr $LSB_JOBINDEX --device cpu --latent-dim 2 --epochs 100 --batch-size 64
 
 
 
