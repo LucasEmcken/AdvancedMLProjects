8645ff4890091154921abefa328f0d457c439ec3
Lucas Sylvester <lucassylvester02@gmail.com>
2025-04-30 22:24:08 +0200

    restructure old project and add new

---
+++ baseline.py
@@ -0,0 +1,6 @@
+# %%
+import torch
+from torch.utils.data import random_split
+from torch_geometric.datasets import TUDataset
+from torch_geometric.loader import DataLoader
+import matplotlib.pyplot as plt
