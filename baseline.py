690ac47e5ff0054bd088621ec42370629393791a
Lucas Sylvester <lucassylvester02@gmail.com>
2025-04-30 23:32:27 +0200

    novel and uniqueness

--- baseline.py
+++ baseline.py
@@ -20,6 +20,13 @@ print(to_networkx(dataset[2],to_undirected=True))
 rng = torch.Generator().manual_seed(0)
 train_dataset, validation_dataset, test_dataset = random_split(dataset, (100, 44, 44), generator=rng)
 
+def convert_to_nx(data):
+    """Convert a PyTorch Geometric data object to a NetworkX graph."""
+    output = []
+    for i in range(len(data)):
+        G = to_networkx(data[i], to_undirected=True)
+        output.append(G)
+    return output
 
 class baseline:
     def __init__(self, train_dataset):
@@ -33,13 +40,27 @@ class baseline:
         self.r = nr_edges / nr_of_possible_edges
     def sample(self):
         # Sample a batch of data from the dataset
-        N = self.train_dataset[random.randint(0, len(self.train_dataset))].num_nodes
+        N = self.train_dataset[random.randint(0, len(self.train_dataset))-1].num_nodes
         sample = nx.erdos_renyi_graph(N,self.r)
         return sample
+    def sample_batch(self, batch_size):
+        # Sample a batch of data from the dataset
+        samples = []
+        for i in range(batch_size):
+            samples.append(self.sample())
+        return samples
 
 # %%
 
-sampler = baseline(train_dataset)
-sample = sampler.sample()
-print(sample)
+train_dataset_nx = convert_to_nx(train_dataset)
+baseline_samples = baseline(train_dataset).sample_batch(10000)
+
+# %%
+print(baseline_samples)
+print("Percentage of unique samples:")
+print(len(set(baseline_samples))/len(baseline_samples))
+print("Percentage of novel samples:")
+print(len([i for i in baseline_samples if i not in train_dataset_nx])/len(baseline_samples))
+print("Percentage of novel and unique samples:")
+print(len(set(baseline_samples) - set(train_dataset_nx))/len(baseline_samples))
 
